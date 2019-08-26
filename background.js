ef3962874fce6e1caf4cb38df908497f8d13c56a
RenzHoly <renzholy@gmail.com>
2019-08-26 17:29:15 +0800

    support png

--- background.js
+++ background.js
@@ -2,25 +2,37 @@ let urls = []
 
 chrome.webRequest.onCompleted.addListener(
   msg => {
-    console.log(msg)
     if (msg.initiator === 'https://musescore.com' && msg.url) {
-      urls.push(msg.url.replace(/\?.*/, ''))
+      urls.push(msg.url.replace(/[\?@].*/, ''))
     }
   },
   {
-    urls: ['*://musescore.com/static/musescore/scoredata/gen/*.svg*'],
+    urls: [
+      '*://musescore.com/static/musescore/scoredata/gen/*.svg*',
+      '*://musescore.com/static/musescore/scoredata/gen/*.png*',
+    ],
   },
   ['responseHeaders'],
 )
 
 chrome.pageAction.onClicked.addListener(async tab => {
   urls = [...new Set(urls)].sort()
-  const svgs = await Promise.all(urls.map(url => fetch(url).then(response => response.text())))
   const doc = new PDFDocument({ autoFirstPage: false })
   const stream = doc.pipe(blobStream())
-  for (const svg of svgs) {
-    doc.addPage()
-    SVGtoPDF(doc, svg.replace(/width="\d+px"/, '').replace(/height="\d+px"/, ''), 0, 0)
+  if (urls[0] && urls[0].endsWith('.svg')) {
+    const svgs = await Promise.all(urls.map(url => fetch(url).then(response => response.text())))
+    for (const svg of svgs) {
+      doc.addPage()
+      SVGtoPDF(doc, svg.replace(/width="\d+px"/, '').replace(/height="\d+px"/, ''), 0, 0)
+    }
+  } else if (urls[0] && urls[0].endsWith('.png')) {
+    const pngs = await Promise.all(
+      urls.map(url => fetch(url).then(response => response.arrayBuffer())),
+    )
+    for (const png of pngs) {
+      doc.addPage()
+      doc.image(png, 0, 0, { fit: [612, 792] })
+    }
   }
   doc.end()
   stream.on('finish', () => {
@@ -48,6 +60,3 @@ chrome.runtime.onInstalled.addListener(() => {
     ])
   })
 })
-
-console.log(SVGtoPDF)
-console.log(PDFDocument)
