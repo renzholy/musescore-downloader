fe388d1f00c6595b95210b2b3c527b6c1eebfda1
RenzHoly <renzholy@gmail.com>
2019-08-26 17:02:27 +0800

    render pdf

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# musescore-downloader
- 
+# MuseScore Downloader
+
+## Example Page
+
+https://musescore.com/user/2116786/scores/4834377
--- background.js
+++ background.js
@@ -13,10 +13,20 @@ chrome.webRequest.onCompleted.addListener(
   ['responseHeaders'],
 )
 
-chrome.pageAction.onClicked.addListener(tab => {
-  // chrome.downloads.download({ url: msg.url })
-  console.log(urls)
-  urls = []
+chrome.pageAction.onClicked.addListener(async () => {
+  urls = [...new Set(urls)].sort()
+  const svgs = await Promise.all(urls.map(url => fetch(url).then(response => response.text())))
+  const doc = new PDFDocument({ autoFirstPage: false })
+  const stream = doc.pipe(blobStream())
+  for (const svg of svgs) {
+    doc.addPage()
+    SVGtoPDF(doc, svg.replace(/width="\d+px"/, '').replace(/height="\d+px"/, ''), 0, 0)
+  }
+  doc.end()
+  stream.on('finish', () => {
+    const url = stream.toBlobURL('application/pdf')
+    chrome.downloads.download({ url })
+  })
 })
 
 chrome.runtime.onInstalled.addListener(() => {
