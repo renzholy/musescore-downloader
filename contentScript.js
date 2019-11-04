17feef810798592c79dd87d953e413dfe866012b
RenzHoly <renzholy@gmail.com>
2019-11-04 10:39:21 +0800

    2.0.2

--- contentScript.js
+++ contentScript.js
@@ -1,13 +1,19 @@
 ;(function() {
   const timer = setInterval(() => {
-    const root = document.querySelector('article section div:nth-child(3) section section')
+    let root = document.querySelector('#download-modal .modal-body .score-download-list__items')
+    let array = Array.from(root.children)
     if (root) {
       clearInterval(timer)
     } else {
-      return
+      root = document.querySelector('article section div:nth-child(3) section section')
+      array = Array.from(root.children).map(({ children }) => children[0].children[0])
+      if (root) {
+        clearInterval(timer)
+      } else {
+        return
+      }
     }
-    Array.from(root.children)
-      .map(({ children }) => children[0].children[0])
+    array
       .filter(({ nodeName }) => nodeName === 'H3')
       .forEach(h3 => {
         const type = h3.innerText
